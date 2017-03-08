489fc26367e1e901bd4c8475ed08da0b32307659
Peiran <hncpr1992@gmail.com>
2017-03-08 15:15:50 -0600

    update global.R

--- global.R
+++ global.R
@@ -1,7 +1,6 @@
 library(ggplot2)
 library(scales)
 library(Hmisc)
-setwd("~/repo/JobParser/parser/user")
 jobData = read.csv("./data/jobFrame.csv",header = T)
 names(jobData)[2] = "c++"
 state = levels(jobData$state)
