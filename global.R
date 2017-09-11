2f0234b2257ed17a74d787086bbae00a96941f40
Peiran <hncpr1992@gmail.com>
2017-09-11 10:37:11 -0500

    modify working directory

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(scales)
 library(Hmisc)
-jobData = read.csv("./data/jobFrame.csv",header = T)
+jobData = read.csv("data/jobFrame.csv",header = T)
 names(jobData)[2] = "c++"
 state = levels(jobData$state)
 skill = names(jobData)[-1]
